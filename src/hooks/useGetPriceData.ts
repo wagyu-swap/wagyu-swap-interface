2859343bb5bb0821bb0b8dc49be259b897fd8dcf
danny-jin <danny@cabinetex.com>
2021-07-04 05:40:07 +0900

    update: tokens api

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -14,67 +14,28 @@ type ApiResponse = {
   }
 }
 
-// const api = 'https://api.wagyuswap.info/api/tokens'
-const api = 'https://explorer.Velas.com/ticker'
-// const api = 'http://localhost:3000/api/tokens'
-
-// data:
-//   0x0C038Bc02C8C9945f194b17C703f800526900A74:
-// name: "SuperNova Protocol"
-// price: "0.01803998730130641259134893653591999"
-// price_VLX: "0.00004547690090850844524878310961274202"
-// symbol: "SUPERN"
-// updated_at:
-
-const convertTokenPrices = (prices: any): ApiResponse => {
-  const response = {
-    update_at: '',
-    data: Object
-  };
-  response.update_at = new Date().getTime().toString();
-  response.data[`${WAGYU.address}`] = {
-    name: WAGYU.name || '',
-    symbol: WAGYU.symbol || '',
-    price: '1.000',
-    price_VLX: (prices.usdt_price / prices.price_usd).toString()
-  }
-  response.data[`${VUSDT.address}`] = {
-    name: VUSDT.name || '',
-    symbol: VUSDT.symbol || '',
-    price: prices.usdt_price.toString(),
-    price_VLX: (prices.usdt_price / prices.price_usd).toString()
-  }
-  response.data[`${VETHER.address}`] = {
-    name: VETHER.name || '',
-    symbol: VETHER.symbol || '',
-    price: prices.eth_price.toString(),
-    price_VLX: (prices.eth_price / prices.price_usd).toString()
-  }
-  response.data[`${VBNB.address}`] = {
-    name: VBNB.name || '',
-    symbol: VBNB.symbol || '',
-    price: (prices.eth_price / 6).toString(),
-    price_VLX: (prices.eth_price / 6 / prices.price_usd).toString()
-  }
-  // @ts-ignore
-  return response
-}
+const api = 'https://wagyu-api.vercel.app/api/tokens'
 
 const useGetPriceData = () => {
   const [data, setData] = useState<ApiResponse | null>(null)
 
   useEffect(() => {
+    let isSubscribed = true;
     const fetchData = async () => {
       try {
         const response = await fetch(api)
         const res: ApiResponse = await response.json()
-        setData(convertTokenPrices(res))
+        if (isSubscribed) {
+          setData(res)
+        }
       } catch (error) {
         console.error('Unable to fetch price data:', error)
       }
     }
-
-    fetchData()
+    fetchData().then();
+    return() => {
+      isSubscribed = false;
+    }
   }, [setData])
 
   return data
